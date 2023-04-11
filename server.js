75721d6ec6c321763cc2087bd4cabb183612b0b5
xybai0103 <xueyin0103@gmail.com>
2023-04-11 11:48:36 -0500

    create a fsUtils.js file within a helpers folder. Define and export three functions: readFromFile, writeToFile, and readAndAppend

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.port || 3001;
 
 const app = express();
 
-//Middleware for parsing JSON and urlencoded form data
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
